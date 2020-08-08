b7aac7142dc1c391b511070d8e1700b3d40f2d60
Paul Ashraf <paulashraf14@gmail.com>
2020-08-08 06:24:00 +0200

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,8 @@
+
+#importing some useful packages
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+%matplotlib inline
+print("Python Notebook works correctly!")
